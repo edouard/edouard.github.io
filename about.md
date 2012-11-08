42f59f1a59ebd2afe9bd8a9ef06e5374d64cddf8
Édouard Brière <edouard.briere@gmail.com>
2012-11-08 09:01:47 +0100

    First commit

---
+++ about.md
@@ -0,0 +1,18 @@
+---
+layout: page
+title: About
+---
+
+<img src="/images/edouard_briere.jpg" style="position: relative; float: right; border: 6px solid #FFF; width: 150px; margin-left: 10px" alt="Photo of Édouard Brière">
+
+I’m Édouard Brière, and I’m a web developer based in Nantes, France.
+
+I currently work on [WebTranslateIt.com](https://webtranslateit.com), a localization software I created in 2009.
+
+From 2007 through 2009 I worked as a web developer and internationalization engineer for [Last.fm](http://last.fm), a social music platform based in United Kingdom.
+
+I also lived for about 3 years in Sweden and I hold a Masters Degree in Computer Sciences from the [University of Linköping](http://en.wikipedia.org/wiki/Linköping_University).
+
+— Autumn 2012
+
+---
---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+set :application,       'foo'
+set :repository,        '_site'
+set :scm,               :none
+set :deploy_via,        :copy
+set :copy_compression,  :gzip
+set :use_sudo,          false
+set :host,              'gandalf.atelierconvivialite.com'
+
+role :web,  host
+role :app,  host
+role :db,   host, :primary => true
+
+set :user,    'edouard'
+set :group,   user
+
+set :deploy_to,    "/web/sites/edouardbriere.fr/"
+
+before 'deploy:update', 'deploy:update_jekyll'
+
+namespace :deploy do
+
+  [:start, :stop, :restart, :finalize_update].each do |t|
+    desc "#{t} task is a no-op with jekyll"
+    task t, :roles => :app do ; end
+  end
+  
+  desc 'Run jekyll to update site before uploading'
+  task :update_jekyll do
+    %x(rm -rf _site/* && jekyll)
+  end
+  
+end
